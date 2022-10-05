9700db595e51226c784431f08d9c275838dc7308
SonSonder <maceomaddox@gmail.com>
2022-10-04 20:58:58 -0400

    added add functions

--- src/getData.js
+++ src/getData.js
@@ -57,3 +57,124 @@ async function getEmployees() {
     connection.end();
 }
 
+async function addDepartment() {
+    const connection = await createConnection();
+    const { dName } = await inquirer.prompt([
+      {
+        type: "input",
+        message: "Department name?",
+        name: "dName"
+      }
+    ]);
+  
+    const [result] = await connection.query(
+      `INSERT INTO department(name) values('${dName}')`
+    );
+  
+    console.log(
+      `Added ${dName} to the database. Department id: ${result.insertId}`
+    );
+    connection.end();
+}
+
+async function addRole() {
+    const connection = await createConnection();
+
+    const { title, salary, dName } = await inquirer.prompt([
+        {
+            type: "input",
+            message: "What is the role?",
+            name: "title"
+        },
+        {
+            type: "input",
+            message: "What is the salary of the role?",
+            name: "salary"
+        },
+        {
+            type: "input",
+            message: "Which department does the role belong to?",
+            name: "dName"
+        },
+    ])
+
+    const [result] = await connection.query(
+        `SELECT d.id FROM department AS d WHERE d.name='${dName}'`
+    );
+    if (!result || !result.length) {
+        console.log(`Failure::The department ${dName} is not in database.`);
+        return;
+    }
+    
+    const [insertRes] = await connection.query(
+        `INSERT INTO role(title,salary,department_id) 
+        values ('${title}', ${salary}, ${result[0].id})`
+    );
+    
+}
+
+async function addEmployee() {
+    const connection = await createConnection();
+
+    const { first_name, last_name, role, manager } = await inquirer.prompt([
+        {
+            type: "input",
+            message: "What is the employee's first name?",
+            name: "first_name"
+        },
+        {
+            type: "input",
+            message: "What is the employee's last name?",
+            name: "last_name"
+        },
+        {
+            type: "input",
+            message: "What is the employee's role?",
+            name: "role"
+        },
+        {
+            type: "input",
+            message: "Who is the employee's manager?",
+            name: "manager"
+        }
+    ]);
+  
+    let roleId;
+    const [rolRes] = await connection.query(
+        `SELECT r.id FROM role AS r WHERE r.title='${role}'`
+    );
+    if (!rolRes || !rolRes.length) {
+        console.log(`Failure::The role ${role} is not in database.`);
+        return;
+    }
+    roleId = rolRes[0].id;
+  
+    let managerId;
+    if (manager) {
+        const man_firstName = manager.split(" ")[0];
+
+        const man_lastName = manager.split(" ")[1];
+  
+        const [result] = await connection.query(
+            `SELECT e.id FROM employee AS e WHERE e.first_name='${man_firstName}' and e.last_name='${man_lastName}'`
+        );
+        if (result && !result.length) {
+            console.log(`Failure::The manager ${manager} is not in database.`);
+            return;
+        }
+        managerId = result[0].id;
+    }
+
+    const [insertRes] = await connection.query(
+        `INSERT INTO employee(first_name,last_name,role_id,manager_id)
+          values ('${first_name}', '${last_name}',${roleId}, ${
+          managerId ? managerId : null
+        })`
+    );
+    
+    console.log(
+        `Added ${first_name} ${last_name} to the database. Employee id: ${insertRes.insertId}`
+    );
+    connection.end();
+}
+
